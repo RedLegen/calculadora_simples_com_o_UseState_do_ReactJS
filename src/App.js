0792d5efb4cd2af99edce467cf8db32474461df5
RedLegen <redlegen@hotmail.com>
2020-08-15 09:40:09 -0300

    calculadora simples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [total, setTotal] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Você clicou {count} vezes</h1>
+
+      <button onClick={() => setCount(count + 1)} className="countClicks">Clica aqui</button>
+      <button onClick={() => setCount(count + 2)} className="countClicks2">Clica aqui para somar 2X</button>
+      <button onClick={() => setCount(count + 3)} className="countClicks3">Clica aqui para somar 3X</button>
+      <button onClick={() => setCount(count + 4)} className="countClicks4">Clica aqui para somar 4X </button>
+
+      <h1>O resultado se somar o {count} + 10</h1>
+
+      <button onClick={() => setTotal( count + 10)} className="somar"> clica aqui para somar</button>
+
+      <p>Total: { total }</p>
     </div>
   );
 }
